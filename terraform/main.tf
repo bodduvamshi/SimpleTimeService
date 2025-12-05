7ee0e58ddc1cfa878c48a4e6cf01b35b3bac6452
bodduvamshi <vamshi.boddu2001@gmail.com>
2025-12-05 13:35:43 +0530

    Initial commit for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.6.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.24"   # Stable latest version as of 2025
     }
 
     kubernetes = {
@@ -15,21 +15,5 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
-}
-
-# Kubernetes provider uses EKS cluster endpoint
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
+  region = var.region
 }
